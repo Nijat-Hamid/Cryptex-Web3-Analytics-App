7e04ba5810bbb57bdbf97593d39c44804bb09765
Nijat Hamid <nicatorium@gmail.com>
2024-12-17 16:00:31 +0400

    Chart Maker Offset calculation is done

---
+++ Cryptex/Common/Components/Chart/LineChart.swift
@@ -0,0 +1,21 @@
+//
+//  LineChartView.swift
+//  Cryptex
+//
+//  Created by Nijat Hamid on 12/16/24.
+//  Copyright © 2024 Nijat Hamid. All rights reserved.
+//
+
+import UIKit
+import DGCharts
+
+class LineChartView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
--- Cryptex/Common/Components/Chart/LineChartView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LineChartVieü.swift
-//  Cryptex
-//
-//  Created by Nijat Hamid on 12/16/24.
-//  Copyright © 2024 Nijat Hamid. All rights reserved.
-//
-
