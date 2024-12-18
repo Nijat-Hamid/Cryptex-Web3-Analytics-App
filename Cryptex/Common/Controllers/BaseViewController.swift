975db4886bb93d9ca85694e6bff07f01c9da79dd
Nijat Hamid <nicatorium@gmail.com>
2024-12-18 15:26:59 +0400

    NavigationController Delegate's lifecycle improved with viewWillAppear

--- Cryptex/Common/Controllers/BaseViewController.swift
+++ Cryptex/Common/Controllers/BaseViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class BaseViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        navigationController?.delegate = self
-    }
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            navigationController?.delegate = self
+        }
+    
     override func loadView() {
         super.loadView()
         setupUI()
