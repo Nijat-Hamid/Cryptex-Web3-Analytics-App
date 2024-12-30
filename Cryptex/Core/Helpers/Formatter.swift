0007c5a00d9ba5d2a800e403b6ab741b07f20b76
Nijat Hamid <nicatorium@gmail.com>
2024-12-30 09:20:18 +0400

    Formatter improved

--- Cryptex/Core/Helpers/Formatter.swift
+++ Cryptex/Core/Helpers/Formatter.swift
@@ -6,18 +6,38 @@
 //  Copyright © 2024 Nijat Hamid. All rights reserved.
 //
 import Foundation
-import UIKit
 
 enum FormatType {
-    case currency 
+    case currency
     case percentage
-    
+    case decimal
 }
 
 struct Formatter {
-    static func format(_ number: Double, as type: FormatType) -> String {
+    static func format(_ number: Double, as type: FormatType,absolute:Bool = false) -> String {
+        let absNumber = abs(number)
+        let sign = number < 0 ? "-" : ""
+        let formattedNumber: String
+        
+        switch absNumber {
+        case 1_000_000_000_000...:
+            formattedNumber = formatValue(absNumber / 1_000_000_000_000, as: type) + "T"
+        case 1_000_000_000...:
+            formattedNumber = formatValue(absNumber / 1_000_000_000, as: type) + "B"
+        case 1_000_000...:
+            formattedNumber = formatValue(absNumber / 1_000_000, as: type) + "M"
+        case 1_000...:
+            formattedNumber = formatValue(absNumber / 1_000, as: type) + "K"
+        default:
+            formattedNumber = formatValue(absNumber, as: type)
+        }
+        
+        let finalReturn = absolute ? "\(formattedNumber)" : "\(sign)\(formattedNumber)"
+        return finalReturn
+    }
+    
+    private static func formatValue(_ value: Double, as type: FormatType) -> String {
         let formatter = NumberFormatter()
-        formatter.numberStyle = .currency
         formatter.maximumFractionDigits = 2
         formatter.minimumFractionDigits = 0
         
@@ -27,26 +47,13 @@ struct Formatter {
             formatter.currencyCode = "USD"
         case .percentage:
             formatter.numberStyle = .percent
+            return formatter.string(for: value / 100) ?? "N/A"
+        case .decimal:
+            formatter.numberStyle = .decimal
+            
         }
         
-        let absNumber = abs(number)
-        let sign = absNumber < 0 ? "-" : ""
-        
-        switch absNumber {
-        case 1_000_000_000_000...:
-            return "\(sign)\(formatter.string(for: absNumber / 1_000_000_000_000) ?? "")T"
-        case 1_000_000_000...:
-            return "\(sign)\(formatter.string(for: absNumber / 1_000_000_000) ?? "")B"
-        case 1_000_000...:
-            return "\(sign)\(formatter.string(for: absNumber / 1_000_000) ?? "")M"
-        case 1_000...:
-            return "\(sign)\(formatter.string(for: absNumber / 1_000) ?? "")K"
-        default:
-            if type == .percentage {
-                let formattedPercentage = formatter.string(for: absNumber / 100) ?? ""
-                return "\(sign)\(formattedPercentage)"
-            }
-            return "\(sign)\(formatter.string(for: absNumber) ?? "")"
-        }
+        return formatter.string(for: value) ?? "N/A"
     }
 }
+
