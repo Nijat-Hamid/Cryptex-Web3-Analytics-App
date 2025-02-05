6a9e74f2956bf749092152bdf1921c4baa651695
Nijat Hamid <nicatorium@gmail.com>
2025-02-05 22:05:15 +0400

    Extensions/UIImage tests added

---
+++ Tests/Extensions/UIImageTests.swift
@@ -0,0 +1,28 @@
+//
+//  UIImageTests.swift
+//  Cryptex
+//
+//  Created by Nijat Hamid on 2/5/25.
+//  Copyright © 2025 Nijat Hamid. All rights reserved.
+//
+
+import Testing
+import UIKit
+
+@Suite
+struct UIImageTests {
+    @Test
+    func testResizedImage() async throws {
+        
+        let originalImage = UIGraphicsImageRenderer(size: CGSize(width: 100, height: 100)).image { context in
+            UIColor.red.setFill()
+            context.fill(CGRect(x: 0, y: 0, width: 100, height: 100))
+        }
+        
+        let targetSize = CGSize(width: 50, height: 50)
+        let resizedImage = originalImage.resizedImage(Size: targetSize)
+        
+        #expect(resizedImage != nil)
+        #expect(resizedImage?.size == targetSize)
+    }
+}
