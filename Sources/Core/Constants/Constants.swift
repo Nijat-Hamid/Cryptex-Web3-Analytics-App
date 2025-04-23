e1ab8fbf1831ab7d66da3be3e6821964a6b45583
Nijat Hamid <nicatorium@gmail.com>
2025-04-23 18:44:37 +0400

    Readme: Changes added

--- Sources/Core/Constants/Constants.swift
+++ Sources/Core/Constants/Constants.swift
@@ -9,5 +9,5 @@
 // MARK: - I 
 struct Constants {
     static let baseImgUrl = "https://storage.googleapis.com/cryptex-images"
-    static let baseApiUrl = "risk-backend-397820138920.europe-west1.run.app"
+    static let baseApiUrl = "risk-backend-397820138920.us-central1.run.app"
 }
